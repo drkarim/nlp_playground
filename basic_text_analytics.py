42bf8a2e991bab269a11287ba75c6b7e7fbd8bf9
Rashed karim <rashed.karim@gmail.com>
2020-02-20 21:47:46 +0000

    first commit

---
+++ basic_text_analytics.py
@@ -0,0 +1,87 @@
+#%% 
+#Loading NLTK
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+
+
+text="""Hello Mr. Smith, how are you doing today? The weather is great, and city is awesome.
+The sky is pinkish-blue. You shouldn't eat cardboard"""
+
+#%% 
+# Sentence tokeniser 
+tokenized_text=sent_tokenize(text)
+
+# word tokeniser 
+tokenized_word=word_tokenize(text)
+
+#%% 
+# Frequency distribution 
+fdist = FreqDist(tokenized_word)
+
+# most common word
+fdist.most_common(2)
+
+#%% 
+# Frequency Distribution Plot
+import matplotlib.pyplot as plt
+fdist.plot(30,cumulative=False)
+plt.show()
+
+#%%
+
+# Stop words 
+stop_words=set(stopwords.words("english"))
+print(stop_words)
+
+#%% 
+# filtering stop-words 
+filtered_sent=[]
+for w in tokenized_word:
+    if w.lower() not in stop_words:
+        filtered_sent.append(w)
+print("Tokenized Sentence:",tokenized_word)
+print("Filterd Sentence:",filtered_sent)
+
+#%% 
+# Stemming
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+ps = PorterStemmer()
+
+stemmed_words=[]
+for w in filtered_sent:
+    stemmed_words.append(ps.stem(w))
+
+print("Filtered Sentence:",filtered_sent)
+print("Stemmed Sentence:",stemmed_words)
+
+#%% 
+#Lexicon Normalization
+#performing stemming and Lemmatization
+from nltk.stem.wordnet import WordNetLemmatizer
+lem = WordNetLemmatizer()
+
+from nltk.stem.porter import PorterStemmer
+stem = PorterStemmer()
+
+words = ["catching", "flying", "truncate"] 
+words_lemmas = [] 
+words_stems = []
+for w in words: 
+    words_lemmas.append(lem.lemmatize(w,pos="v"))
+    words_stems.append(stem.stem(w))
+
+print("lemma = ",words_lemmas)
+print("stemmer = ",words_stems)
+
+
+#%%
+sent = "Albert Einstein was born in Ulm, Germany in 1879."
+tokens=nltk.word_tokenize(sent)
+#print(tokens)
+nltk.pos_tag(tokens)
+
---
+++ tex_features.py
@@ -0,0 +1,47 @@
+#%%
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import RegexpTokenizer
+from sklearn.model_selection import train_test_split
+
+
+#%%
+import pandas as pd
+data=pd.read_csv('train.tsv', sep='\t')
+data.head()
+data.info()
+data.Sentiment.value_counts()
+
+#%%
+import matplotlib.pyplot as plt
+Sentiment_count=data.groupby('Sentiment').count()
+plt.bar(Sentiment_count.index.values, Sentiment_count['Phrase'])
+plt.xlabel('Review Sentiments')
+plt.ylabel('Number of Review')
+plt.show()
+
+#%%
+#tokenizer to remove unwanted elements from out data like symbols and numbers
+token = RegexpTokenizer(r'[a-zA-Z0-9]+')
+cv = CountVectorizer(lowercase=True,stop_words='english',ngram_range = (1,1),tokenizer = token.tokenize)
+text_counts= cv.fit_transform(data['Phrase'])
+
+#%%
+# Feature Generation using Bag of Words
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    text_counts, data['Sentiment'], test_size=0.3, random_state=1)
+
+# %%
+# Now lets build Text Classification Model using BoW features
+from sklearn.naive_bayes import MultinomialNB
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
+# Model Generation Using Multinomial Naive Bayes
+clf = MultinomialNB().fit(X_train, y_train)
+predicted= clf.predict(X_test)
+print("MultinomialNB Accuracy:",metrics.accuracy_score(y_test, predicted))
+
+# %%
+from sklearn.feature_extraction.text import TfidfVectorizer
+tf=TfidfVectorizer()
+text_tf= tf.fit_transform(data['Phrase'])
